03d3d43b72b8a3cc98362879e9d771f831118a0e
pheodc <phelipe.odc@hotmail.com>
2017-01-08 23:38:23 -0200

    Criação Integração Facebook - 2

--- app/src/main/java/android/politicar/com/br/politicar/Activitys/LoginActivity.java
+++ app/src/main/java/android/politicar/com/br/politicar/Activitys/LoginActivity.java
@@ -1,20 +1,35 @@
 package android.politicar.com.br.politicar.Activitys;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.politicar.com.br.politicar.R;
 import android.politicar.com.br.politicar.WebService.CarregaDadosWS;
+import android.politicar.com.br.politicar.WebService.CustomJsonObjectRequest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -26,8 +41,28 @@ public class LoginActivity extends AppCompatActivity {
     EditText senha;
     HashMap<String, String> params;
     LoginButton loginButton;
+    //int idUsuario;
+
+    String nomeFace;
+    String idFace;
+    String emailFace;
+    String birthdayFace;
+    String generoFace;
+
+    String nomeUsuario;
+    String emailUsuario;
+    String urlImagem;
+    String cadastroFacebook;
+    String idusuarioFacebbok;
+    String generoUsuario;
     int idUsuario;
+
+    RequestQueue rq;
+
+    CallbackManager callbackManager;
+
     String URL = "http://52.40.192.10/Politicar/loginPoliticar.php";
+    String URLFACE = "http://52.40.192.10/Politicar/cadastroFacebook.php";
     CarregaDadosWS carregaDados;
 
     private static final String SESSAO = "SessaoPoliticar";
@@ -36,7 +71,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-
+        rq = Volley.newRequestQueue(this);
         SharedPreferences sessao = getSharedPreferences(SESSAO, MODE_PRIVATE);
 
         idUsuario = sessao.getInt("id", 0);
@@ -50,10 +85,16 @@ public class LoginActivity extends AppCompatActivity {
         login = (EditText) findViewById(R.id.editLogin);
         senha = (EditText) findViewById(R.id.editSenha);
 
+        callbackManager = CallbackManager.Factory.create();
+
+
         loginButton = (LoginButton) findViewById(R.id.btnFbLogin);
         loginButton.setReadPermissions(Arrays.asList("public_profile","email", "user_birthday"));
 
+        botaoFace();
+
     }
+
 
     public void validaLogin(View view){
         params = new HashMap<String, String>();
@@ -67,5 +108,162 @@ public class LoginActivity extends AppCompatActivity {
     public void abrirCadastro(View view){
         Intent intent = new Intent(this, CadastroActivity.class);
         startActivity(intent);
+    }
+
+    public void botaoFace(){
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Profile profile = Profile.getCurrentProfile();
+                // Log.v("Script Facebook:",profile.getName());
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                Log.v("LoginActivity", response.toString());
+                                params = new HashMap<String, String>();
+                                // Application code
+                                try {
+                                    nomeFace = object.getString("name");
+
+                                    Log.v("Script_Face", nomeFace);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    emailFace = object.getString("email");
+                                    //Log.v("Script_Face", emailFace);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    idFace = object.getString("id");
+
+                                    Log.v("Script_Face", idFace);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    birthdayFace = object.getString("birthday"); // 01/31/1980 format
+
+                                    Log.v("Script_Face", birthdayFace);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                try {
+                                    generoFace = object.getString("gender"); // 01/31/1980 format
+
+                                    Log.v("Script_Face", generoFace);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                params.put("nomeCompleto", nomeFace);
+                                params.put("login", emailFace);
+                                params.put("idFace", idFace);
+                                params.put("dataNascimento", birthdayFace);
+                                params.put("genero", generoFace);
+                                //Log.v("Script_Face", emailFace);
+
+                                //carregaDados = new CarregaDadosWS();
+                                //carregaDados.loginFace(URLFACE, LoginActivity.this, params, LoginActivity.this);
+
+                                //RequestQueue rq = Volley.newRequestQueue(this);
+                                CustomJsonObjectRequest requestFace = new CustomJsonObjectRequest(Request.Method.POST, URLFACE, params, new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        SharedPreferences sessao = getSharedPreferences(SESSAO, MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sessao.edit();
+                                        Log.i("Script, ", "SUCCESS" + response);
+                                        try {
+
+                                            String resposta = response.getString("message");
+
+
+                                            if (resposta.equals("Ok")) {
+
+                                                idUsuario = response.getInt("id");
+                                                nomeUsuario = response.getString("nomeUsuario");
+                                                emailUsuario = response.getString("loginUsuario");
+                                                urlImagem = response.getString("imagemUsuario");
+                                                generoUsuario = response.getString("generoUsuario");
+                                                cadastroFacebook = response.getString("cadastroFacebook");
+
+                                                editor.putInt("id", idUsuario);
+                                                editor.putString("nomeUsuario", nomeUsuario);
+                                                editor.putString("loginUsuario", emailUsuario);
+                                                editor.putString("imagemUsuario", urlImagem);
+                                                editor.putString("cadastroFacebook", cadastroFacebook);
+                                                editor.commit();
+
+                                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                idUsuario = response.getInt("id");
+                                                nomeUsuario = response.getString("nomeUsuario");
+                                                emailUsuario = response.getString("loginUsuario");
+                                                generoUsuario = response.getString("generoUsuario");
+                                                urlImagem = response.getString("imagemUsuario");
+                                                cadastroFacebook = response.getString("cadastroFacebook");
+
+                                                editor.putInt("id", idUsuario);
+                                                editor.putString("nomeUsuario", nomeUsuario);
+                                                editor.putString("loginUsuario", emailUsuario);
+                                                editor.putString("imagemUsuario", urlImagem);
+                                                editor.putString("generoUsuario", generoUsuario);
+                                                editor.putString("cadastroFacebook", cadastroFacebook);
+                                                editor.commit();
+
+                                                Toast.makeText(getApplicationContext(), resposta, Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+
+                                    }
+                                }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Toast.makeText(getApplicationContext(), "Erro na Conexão", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                requestFace.setTag("tag");
+                                rq.add(requestFace);
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/android/politicar/com/br/politicar/WebService/CarregaDadosWS.java
+++ app/src/main/java/android/politicar/com/br/politicar/WebService/CarregaDadosWS.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.politicar.com.br.politicar.Activitys.HomeActivity;
 import android.politicar.com.br.politicar.Activitys.LoginActivity;
 import android.util.Log;
@@ -16,11 +17,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -37,14 +44,14 @@ public class CarregaDadosWS {
     String urlImagem;
     String cadastroFacebook;
     String idusuarioFacebbok;
+    String generoUsuario;
     int idUsuario;
 
-    CallbackManager callbackManager;
-    LoginButton loginButton;
-    String nomeFace;
-    String idFace;
-    String emailFace;
-    String birthdayFace;
+
+
+
+    HashMap<String, String> params;
+
     private static final String SESSAO = "SessaoPoliticar";
 
     public interface ResultListener {
@@ -53,7 +60,7 @@ public class CarregaDadosWS {
 
     private ResultListener listener;
 
-    public void cadastroVolley(String url, final Context context, Map<String,String> params, final Activity activity){
+    public void cadastroVolley(String url, final Context context, Map<String, String> params, final Activity activity) {
         RequestQueue rq = Volley.newRequestQueue(context);
 
 
@@ -64,12 +71,12 @@ public class CarregaDadosWS {
                 try {
 
                     String resposta = response.getString("message");
-                    if(resposta.equals("Ótimo, Seu Cadastro está Pronto, Divirta-se!")){
-                        carregando = ProgressDialog.show(context,"Criando seu Cadastro","Carregando...!", false, true);
+                    if (resposta.equals("Ótimo, Seu Cadastro está Pronto, Divirta-se!")) {
+                        carregando = ProgressDialog.show(context, "Criando seu Cadastro", "Carregando...!", false, true);
                         carregando.setCancelable(false);
                         //Toast.makeText(getApplicationContext(), resposta, Toast.LENGTH_SHORT).show();
                         activity.finish();
-                    }else {
+                    } else {
                         Toast.makeText(context, resposta, Toast.LENGTH_SHORT).show();
                     }
 
@@ -89,11 +96,12 @@ public class CarregaDadosWS {
         rq.add(request);
 
     }
-    public void setResultListener(ResultListener listener){
+
+    public void setResultListener(ResultListener listener) {
         this.listener = listener;
     }
 
-    public void loginVolley(String url, final Context context, Map<String,String> params, final Activity activity){
+    public void loginVolley(String url, final Context context, Map<String, String> params, final Activity activity) {
         RequestQueue rq = Volley.newRequestQueue(context);
 
         CustomJsonObjectRequest request = new CustomJsonObjectRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
@@ -105,10 +113,10 @@ public class CarregaDadosWS {
                     String resposta = response.getString("message");
 
 
-                    if(resposta.equals("Ok")){
-                        carregando = ProgressDialog.show(context,"Verificando seu Login","Carregando...!", false, true);
+                    if (resposta.equals("Ok")) {
+                        carregando = ProgressDialog.show(context, "Verificando seu Login", "Carregando...!", false, true);
                         carregando.setCancelable(false);
-                        idUsuario =  response.getInt("id");
+                        idUsuario = response.getInt("id");
                         nomeUsuario = response.getString("nomeUsuario");
                         emailUsuario = response.getString("loginUsuario");
                         idusuarioFacebbok = response.getString("senhaUsuario");
@@ -136,7 +144,7 @@ public class CarregaDadosWS {
                         Intent intent = new Intent(context, HomeActivity.class);
                         activity.startActivity(intent);
                         activity.finish();
-                    }else {
+                    } else {
                         Toast.makeText(context.getApplicationContext(), resposta, Toast.LENGTH_SHORT).show();
 
                     }
@@ -156,5 +164,77 @@ public class CarregaDadosWS {
         request.setTag("tag");
         rq.add(request);
     }
+
+    public void loginFace(String url, final Context context, Map<String, String> params, final Activity activity) {
+        RequestQueue rq = Volley.newRequestQueue(context);
+        CustomJsonObjectRequest requestFace = new CustomJsonObjectRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                SharedPreferences sessao = activity.getSharedPreferences(SESSAO, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sessao.edit();
+                Log.i("Script, ", "SUCCESS" + response);
+                try {
+
+                    String resposta = response.getString("message");
+
+
+                    if (resposta.equals("Ok")) {
+
+                        idUsuario = response.getInt("id");
+                        nomeUsuario = response.getString("nomeUsuario");
+                        emailUsuario = response.getString("loginUsuario");
+                        urlImagem = response.getString("imagemUsuario");
+                        generoUsuario = response.getString("generoUsuario");
+                        cadastroFacebook = response.getString("cadastroFacebook");
+
+                        editor.putInt("id", idUsuario);
+                        editor.putString("nomeUsuario", nomeUsuario);
+                        editor.putString("loginUsuario", emailUsuario);
+                        editor.putString("imagemUsuario", urlImagem);
+                        editor.putString("cadastroFacebook", cadastroFacebook);
+                        editor.commit();
+
+                        Intent intent = new Intent(activity, HomeActivity.class);
+                        context.startActivity(intent);
+                        activity.finish();
+                    } else {
+                        idUsuario = response.getInt("id");
+                        nomeUsuario = response.getString("nomeUsuario");
+                        emailUsuario = response.getString("loginUsuario");
+                        generoUsuario = response.getString("generoUsuario");
+                        urlImagem = response.getString("imagemUsuario");
+                        cadastroFacebook = response.getString("cadastroFacebook");
+
+                        editor.putInt("id", idUsuario);
+                        editor.putString("nomeUsuario", nomeUsuario);
+                        editor.putString("loginUsuario", emailUsuario);
+                        editor.putString("imagemUsuario", urlImagem);
+                        editor.putString("generoUsuario", generoUsuario);
+                        editor.putString("cadastroFacebook", cadastroFacebook);
+                        editor.commit();
+
+                        Toast.makeText(context.getApplicationContext(), resposta, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(activity, HomeActivity.class);
+                        context.startActivity(intent);
+                        activity.finish();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context.getApplicationContext(), "Erro na Conexão", Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestFace.setTag("tag");
+        rq.add(requestFace);
+    }
 }
+
+
 
